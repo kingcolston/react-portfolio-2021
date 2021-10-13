41ecb8a42db5e94377565585d97888932c6bec2b
kingcolston <44851415+kingcolston@users.noreply.github.com>
2021-10-12 23:27:27 -0400

    Updates

---
+++ precache-manifest.586b90b8a856a8d3e0d02667498e9129.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfca42793ede31a649d0a998e892fd9",
+    "url": "/react-portfolio-2021/index.html"
+  },
+  {
+    "revision": "f117ea61788dcbe54b05",
+    "url": "/react-portfolio-2021/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8619cd361292f362f8ed",
+    "url": "/react-portfolio-2021/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "f117ea61788dcbe54b05",
+    "url": "/react-portfolio-2021/static/js/2.f7bf02f3.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio-2021/static/js/2.f7bf02f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8619cd361292f362f8ed",
+    "url": "/react-portfolio-2021/static/js/main.c83d0dc7.chunk.js"
+  },
+  {
+    "revision": "c432277223e64a34456e",
+    "url": "/react-portfolio-2021/static/js/runtime-main.1ca44243.js"
+  }
+]);
